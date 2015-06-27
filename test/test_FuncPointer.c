57169a87fca132097332eea8b65dfb34117a1d02
EmeraldWolfram <stevenfgy@yahoo.com>
2015-06-27 14:11:22 +0800

    Create Function Pointer Project

--- test/test_FuncPointer.c
+++ test/test_FuncPointer.c
@@ -1,15 +1,43 @@
 #include "unity.h"
 #include "FuncPointer.h"
+#include <stdio.h>
 
-void setUp(void)
-{
+void setUp(void){}
+
+void tearDown(void){}
+
+void dummy(int value){
+  printf("I am a dummy with value %d\n", value);
 }
 
-void tearDown(void)
-{
-}
+void test_FuncPointer(void){
+  char value = 7;
+  char* ptrToChar;
+  char* ptrToChar1;
+  char** ptrToPtrToChar = NULL;
+  ptrToChar = &value;
+  ptrToPtrToChar = &ptrToChar;
+  
+  void (*FuncPtr)(int) = dummy;         //FuncPtr is pointer to function that take (int) and return void
+  void (**FuncPtrPtr1)(int) = &FuncPtr; //FuncPtrPtr is pointer to pointer to function that take (int) and return void
+  
+  (*FuncPtrPtr1)(40);
+  
+  printf("Address of value is 0x%p\n", &value);
+  printf("Address of ptrToChar is 0x%p\n", &ptrToChar);
+  printf("Address of ptrToChar1 is 0x%p\n", &ptrToChar1);
+  printf("Address of ptrToPtrToChar is 0x%p\n\n", &ptrToPtrToChar);
+  
+  printf("Number of value is 0x%X\n", value);
+  printf("Number of ptrToChar is 0x%X\n", ptrToChar);
+  printf("Number of ptrToChar1 is 0x%X\n", ptrToChar1);
+  printf("Number of ptrToPtrToChar is 0x%X\n\n", ptrToPtrToChar);
 
-void test_module_generator_needs_to_be_implemented(void)
-{
-	TEST_IGNORE_MESSAGE("Implement me!");
+  ptrToPtrToChar = &ptrToChar1;
+  *ptrToPtrToChar = &value;
+  **ptrToPtrToChar = 88;
+  printf("Number of new ptrToPtrToChar is 0x%X\n", ptrToPtrToChar);
+  printf("Number of new ptrToChar is 0x%X\n", ptrToChar);
+  printf("Number of new value is 0x%X\n", value);
+
 }
